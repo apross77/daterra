3a79e44ee9594740a6f0e8aa36d88a85dafe1bb4
root <root@srv770269.hstgr.cloud>
2025-07-03 14:19:48 +0000

    primeiro commit

---
+++ src/config/database.js
@@ -0,0 +1,44 @@
+import firebird from "node-firebird";
+
+const dbOptions = {
+    host: '200.233.207.231',
+    port: 3050,
+    database: 'D:\\FCERTA\\DB\\ALTERDB.IB',
+    user: 'SYSDBA',
+    password: 'masterkey',
+    lowercase_keys: false, // set to true to lowercase keys
+    role: null, // default
+    pageSize: 4096, // default when creating database
+    retryConnectionInterval: 1000, // reconnect interval in case of connection drop
+    blobAsText: false, // set to true to get blob as text, only affects blob subtype 1
+    encoding: 'UTF8', // default encoding for connection is UTF-8    
+};
+
+function executeQuery(ssql, params, callback)
+{
+
+firebird.attach(dbOptions, function (err, db) {
+
+  if (err) 
+    {
+        return callback(err, []);
+    }
+  
+    // db = DATABASE
+    db.query(ssql, params, function (err, result)
+    {
+      
+      db.detach();
+
+      if (err) {
+        return callback(err, [])
+    } else {
+        return callback(undefined, result) ;
+        }
+    })
+
+    });
+  };
+
+
+export {executeQuery};
---
+++ src/index.js
@@ -0,0 +1,68 @@
+import express from "express";
+import cors from "cors";
+import { executeQuery } from "./config/database.js"
+
+const app = express();
+
+// Middleware JSON
+app.use(express.json());
+
+//Middleware CORS
+app.use(cors());
+
+// Rotas
+app.get("/requisicao", function(req, res)
+{
+    let filtro = [];
+    let ssql = "SELECT DISTINCT	req.CDFIL, req.DTENTR, req.NRRQU, req.SERIER, itm.DESCR, req.VOLUME, req.UNIVOL FROM FC12100 req INNER JOIN FC12110 itm	ON (req.CDFIL = itm.CDFIL AND req.NRRQU = itm.NRRQU AND req.SERIER = itm.SERIER) INNER JOIN FC07000 cli ON (req.CDCLI = cli.CDCLI) INNER JOIN FC07200 c ON (cli.CDCLI = c.CDCLI) WHERE req.DTENTR > current_date - 120 and itm.ITEMID = 1 and c.NRDDD > 0 and NRTEL > 0";
+
+    if (req.query.NRDDD){
+        ssql += "and c.NRDDD = ?";
+        filtro.push(req.query.NRDDD);
+    }
+    if (req.query.NRTEL){
+        ssql += "and c.NRTEL = ?";
+        filtro.push(req.query.NRTEL);
+    }
+
+    executeQuery(ssql, filtro, function(err, result)
+    {
+            if (err)
+            {
+                res.status(500).json(err);
+            }
+            else 
+            {
+                res.status(200).json(result);
+            } 
+    }
+)   
+});
+app.get("/pcp", function(req, res)
+{
+    let filtro = [];
+    let ssql = "SELECT req.dtentr, req.cdcli, req.nomepa, req.nrrqu, req.serier, pcp.cdetapa, pcp.data, pcp.hora, eta.descricao FROM FC12100 req LEFT JOIN (SELECT p1.cdfil, p1.nrrqu, p1.serier, p1.cdetapa, p1.data, p1.hora FROM FC12500 p1 JOIN (SELECT p2.nrrqu, p2.serier, p2.cdfil, MAX(CAST(p2.data AS VARCHAR(10)) || ' ' || CAST(p2.hora AS VARCHAR(15))) AS max_datahora FROM FC12500 p2 WHERE p2.cdopera = 01 GROUP BY p2.nrrqu, p2.serier, p2.cdfil) ult ON p1.nrrqu = ult.nrrqu AND p1.serier = ult.serier AND p1.cdfil = ult.cdfil AND (CAST(p1.data AS VARCHAR(10)) || ' ' || CAST(p1.hora AS VARCHAR(15))) = ult.max_datahora WHERE p1.cdopera = 01) pcp ON req.cdfil = pcp.cdfil AND req.nrrqu = pcp.nrrqu AND req.serier = pcp.serier INNER JOIN FC12540 eta ON pcp.cdetapa = eta.cdetapa WHERE req.DTENTR >= current_date - 120 AND req.tpformafarma NOT IN (7, 8) AND req.cdfil = 1 and req.NRRQU > 0";
+
+    if (req.query.NRRQU){
+        ssql += "and req.NRRQU = ?";
+        filtro.push(req.query.NRRQU);
+    }
+
+    executeQuery(ssql, filtro, function(err, result)
+    {
+            if (err)
+            {
+                res.status(500).json(err);
+            }
+            else 
+            {
+                res.status(200).json(result);
+            } 
+    }
+)   
+});
+
+app.listen(3000, '0.0.0.0', function()
+{
+    console.log("Servidor no ar");
+});
